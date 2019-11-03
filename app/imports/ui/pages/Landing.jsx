67bf6d001a9624a038c1af47cef500e84303ae24
yuumayamamoto <46611050+yuumayamamoto@users.noreply.github.com>
2019-11-02 20:05:32 -1000

    not done

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className={'background image'}>
+        <Grid container stackable centered columns = {3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column>
+            <Icon size='huge' name ='users' inverted/>
+            <Header as = 'h1'>Multiple Users</Header>
+            <Header as = 'h3'>Thsi address book enables any number of users
+            to register and save their business contacts. You can only see the contacts you have created.</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Icon size='huge' name ='file alternate' inverted/>
+            <Header as = 'h1'>Contact Details</Header>
+            <Header as = 'h3'>For each contact, you can save their name, address, and phone number.</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Icon size='huge' name ='calendar check' inverted/>
+            <Header as = 'h1'>Timestanped Notes</Header>
+            <Header as = 'h3'>Each time you make </Header>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
 
         </Grid>
+        </div>
     );
   }
 }
